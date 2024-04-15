713122461d0a36179a29e36ad9baa0eb66971c75
Hailey <me@haileyok.com>
2024-04-14 20:12:31 -0700

    use link for fonts

--- src/app/root.tsx
+++ src/app/root.tsx
@@ -12,7 +12,15 @@ import {LinksFunction} from '@remix-run/node'
 import styles from './tailwind.css?url'
 import {Link} from './components/link'
 
-export const links: LinksFunction = () => [{rel: 'stylesheet', href: styles}]
+export const links: LinksFunction = () => [
+  {rel: 'stylesheet', href: styles},
+  {rel: 'preconnect', href: 'https://fonts.googleapis.com'},
+  {rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true},
+  {
+    href: 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap',
+    rel: 'stylesheet',
+  },
+]
 
 export function Layout({children}: {children: React.ReactNode}) {
   return (
