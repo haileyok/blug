07188c6f638fdc6e3f169dedc87048d20dced4b8
Hailey <me@haileyok.com>
2024-04-14 22:04:22 -0700

    remove default title

--- src/app/root.tsx
+++ src/app/root.tsx
@@ -28,7 +28,6 @@ export function Layout({children}: {children: React.ReactNode}) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Hailey's Cool Site</title>
         <Meta />
         <Links />
       </head>
