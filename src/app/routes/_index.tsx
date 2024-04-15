1edaf415f1bbefb26c4311573b8edec58456841e
Hailey <me@haileyok.com>
2024-04-14 19:13:12 -0700

    add back avatar

--- src/app/routes/_index.tsx
+++ src/app/routes/_index.tsx
@@ -34,7 +34,15 @@ export default function Index() {
     <div className="container mx-auto pt-10 md:pt-20 pb-20">
       <div className="flex-col text-center">
         <div className="flex flex-col md:flex-row justify-center items-center gap-4 md:gap-20 pb-10">
-          <div className="w-32 h-32 bg-gray-300 rounded-full"></div>
+          {profile ? (
+            <img
+              className="rounded-full w-32 h-32"
+              src={profile.avatar}
+              alt="Hailey's avatar"
+            />
+          ) : (
+            <div className="w-32 h-32 bg-gray-300 rounded-full"></div>
+          )}
           <h1 className="text-5xl md:text-6xl font-bold">It's Hailey! 👋</h1>
         </div>
         <p className="text-3xl text-theme-300">
